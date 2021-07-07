d6362b20a9753fa32d8e7d49ef851cd872699e1e
Thiago Ferreira <thiagodnf@gmail.com>
2021-07-07 19:35:32 -0400

    add submenu dropdown

--- js/script.js
+++ js/script.js
@@ -47,6 +47,41 @@ $(function () {
         return false;
     }
 
+    document.addEventListener("DOMContentLoaded", function(){
+        // make it as accordion for smaller screens
+        if (window.innerWidth < 992) {
+
+          // close all inner dropdowns when parent is closed
+          document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+            everydropdown.addEventListener('hidden.bs.dropdown', function () {
+              // after dropdown is hidden, then find all submenus
+                this.querySelectorAll('.submenu').forEach(function(everysubmenu){
+                  // hide every submenu as well
+                  everysubmenu.style.display = 'none';
+                });
+            })
+          });
+
+          document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+            element.addEventListener('click', function (e) {
+                let nextEl = this.nextElementSibling;
+                if(nextEl && nextEl.classList.contains('submenu')) {
+                  // prevent opening link if link needs to open dropdown
+                  e.preventDefault();
+                  if(nextEl.style.display == 'block'){
+                    nextEl.style.display = 'none';
+                  } else {
+                    nextEl.style.display = 'block';
+                  }
+
+                }
+            });
+          })
+        }
+        // end if innerWidth
+        });
+        // DOMContentLoaded  end
+
     $("#play").click(() => {canvas.setPlay();});
     $("#step").click(() => { canvas.setStep();});
     $("#stop").click(() => canvas.setStop());
