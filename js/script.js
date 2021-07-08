43ace799c022e002efae3c4fd2b4443533666b61
Thiago Ferreira <thiagodnf@gmail.com>
2021-07-07 20:00:59 -0400

    fixed small issues

--- js/script.js
+++ js/script.js
@@ -82,6 +82,23 @@ $(function () {
         });
         // DOMContentLoaded  end
 
+    // $("ul.dropdown-menu [data-toggle='dropdown']").on("click", function(event) {
+	// 	event.preventDefault();
+	// 	event.stopPropagation();
+
+	// 	$(".dropdown-menu").find('.show').removeClass("show");
+
+	// 	$(this).siblings().toggleClass("show");
+
+	// 	if (!$(this).next().hasClass('show')) {
+	// 		$(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+	// 	}
+
+	// 	$(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
+	// 		$('.dropdown-submenu .show').removeClass("show");
+	// 	});
+	// });
+
     $("#play").click(() => {canvas.setPlay();});
     $("#step").click(() => { canvas.setStep();});
     $("#stop").click(() => canvas.setStop());
