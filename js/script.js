6c6d83bfcb1f10bca689bb82cdc248d9cb8861c2
Thiago Ferreira <thiagodnf@gmail.com>
2021-07-11 12:09:11 -0400

    fixed click on submenu

--- js/script.js
+++ js/script.js
@@ -52,40 +52,45 @@ $(function () {
         return false;
     }
 
-    document.addEventListener("DOMContentLoaded", function(){
+    $(".dropdown-item-no-click").click((event) =>{
+        event.preventDefault();
+        return false;
+    })
+    document.addEventListener("DOMContentLoaded", function () {
+
         // make it as accordion for smaller screens
         if (window.innerWidth < 992) {
 
-          // close all inner dropdowns when parent is closed
-          document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
-            everydropdown.addEventListener('hidden.bs.dropdown', function () {
-              // after dropdown is hidden, then find all submenus
-                this.querySelectorAll('.submenu').forEach(function(everysubmenu){
-                  // hide every submenu as well
-                  everysubmenu.style.display = 'none';
+            // close all inner dropdowns when parent is closed
+            document.querySelectorAll('.navbar .dropdown').forEach(function (everydropdown) {
+                everydropdown.addEventListener('hidden.bs.dropdown', function () {
+                    // after dropdown is hidden, then find all submenus
+                    this.querySelectorAll('.submenu').forEach(function (everysubmenu) {
+                        // hide every submenu as well
+                        everysubmenu.style.display = 'none';
+                    });
+                })
+            });
+
+            document.querySelectorAll('.dropdown-menu a').forEach(function (element) {
+                element.addEventListener('click', function (e) {
+
+                    let nextEl = this.nextElementSibling;
+
+                    if (nextEl && nextEl.classList.contains('submenu')) {
+                        // prevent opening link if link needs to open dropdown
+                        e.preventDefault();
+                        if (nextEl.style.display == 'block') {
+                            nextEl.style.display = 'none';
+                        } else {
+                            nextEl.style.display = 'block';
+                        }
+
+                    }
                 });
             })
-          });
-
-          document.querySelectorAll('.dropdown-menu a').forEach(function(element){
-            element.addEventListener('click', function (e) {
-                let nextEl = this.nextElementSibling;
-                if(nextEl && nextEl.classList.contains('submenu')) {
-                  // prevent opening link if link needs to open dropdown
-                  e.preventDefault();
-                  if(nextEl.style.display == 'block'){
-                    nextEl.style.display = 'none';
-                  } else {
-                    nextEl.style.display = 'block';
-                  }
-
-                }
-            });
-          })
-        }
-        // end if innerWidth
-        });
-        // DOMContentLoaded  end
+        } // end if innerWidth
+    });// DOMContentLoaded  end
 
 
 
